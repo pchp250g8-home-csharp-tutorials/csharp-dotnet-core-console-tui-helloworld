9862109517a2ea8e962c15689c8f88cbfede1ffe
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-02 17:46:32 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.CursorVisible = false;
+Console.CursorTop = 10;
+Console.CursorLeft = 10;
+Console.Write("Hello,World!!!");
+Console.ReadKey();
